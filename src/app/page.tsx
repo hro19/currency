1e144b126c71f544a2f923c06907634c1842a4c2
hori <fourbasic19@gmail.com>
2023-09-27 02:55:18 +0900

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="p-24">
+      <div>
+        <h1 className="text-orange-400">インデックスページ</h1>
+      </div>
+    </main>
+  );
+}
