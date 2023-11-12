49313911b0018dbe8a41e87984e68079fc227bca
hori <fourbasic19@gmail.com>
2023-11-12 23:23:53 +0900

    【jest】設定

---
+++ src/__test__/aaa.test.ts
@@ -0,0 +1,18 @@
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+describe("addNumbers", () => {
+    it("adds two numbers", () => {
+        expect(addNumbers(2, 3)).toBe(5);
+    });
+
+    it("adds negative numbers", () => {
+        expect(addNumbers(-2, -3)).toBe(-5);
+    });
+
+    it("adds decimal numbers", () => {
+        expect(addNumbers(0.1, 0.2)).toBeCloseTo(0.3);
+    });
+});
