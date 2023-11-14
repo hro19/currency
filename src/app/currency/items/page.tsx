add539bada68f09dff97c84a85474b491393affb
hori <fourbasic19@gmail.com>
2023-11-14 13:42:02 +0900

    【next】serverコンポーネントからchakraを排除

--- src/app/currency/items/page.tsx
+++ src/app/currency/items/page.tsx
@@ -8,15 +8,10 @@ import { Item } from '@/ts/Item';
     const data = await fetchItems.getAll()
     
     return (
-      <Box>
-        <Container maxW="1180px">
-        <Heading as={"h1"} className="text-2xl text-blue-500 font-bold my-4 ml-2">テスト開発【item】</Heading>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         {data.map((item: Item) => (
         <ItemCard item={item} key={item.id} />
         ))}
       </div>
-        </Container>
-      </Box>
       )
     }
