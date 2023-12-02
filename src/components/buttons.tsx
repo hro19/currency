7705fbff45fec727b620a2fa82503dc430eb7292
hori <fourbasic19@gmail.com>
2023-12-02 18:21:33 +0900

    【icon】userログイン用のアイコン

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { signIn, signOut } from "next-auth/react";
+import { User } from "lucide-react";
+import { Button } from "@chakra-ui/react";
 
 // ログインボタン
 export const LoginButton = () => {
   return (
-    <button style={{ marginRight: 10 }} onClick={() => signIn()}>
-      Sign in
-    </button>
+    <Button className="bg-slate-200" size="sm" onClick={() => signIn()}>
+      <User size={16} />
+    </Button>
   );
 };
 
 // ログアウトボタン
-export const LogoutButton = () => {
-  return (
-    <button style={{ marginRight: 10 }} onClick={() => signOut()}>
-      Sign Out
-    </button>
-  );
-};
+// export const LogoutButton = () => {
+//   return (
+//     <button style={{ marginRight: 10 }} onClick={() => signOut()}>
+//       Sign Out
+//     </button>
+//   );
+// };
