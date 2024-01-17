6d7f7e987f24b6455e85abdf761260d12526a525
hro19 <fourbasic19@gmail.com>
2024-01-17 18:08:39 +0900

    【next】計算機ページをもとにトップページの土台を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-export default function Home() {
+import React from "react";
+import { CurrencyObj } from "@/ts/Currency";
+import CurrencyWrap from "@/components/currency/CurrencyWrap";
+import { GetApiCurency } from "@/api/currency/getApiCurrency";
+import SelectNational from "@/app/currency/php/SelectNational";
+import HeaderTitle from "@/components/common/HeaderTitle";
+import { Box } from "@chakra-ui/react";
+
+const Home = async () => {
+  const currencyObjData: Awaited<CurrencyObj> = await GetApiCurency("jpy");
+
   return (
-    <main className="p-24">
-      <div>
-        <h1 className="text-orange-400">インデックスページ</h1>
-      </div>
-    </main>
+    <>
+      <HeaderTitle title="通貨記録 アプリ" />
+      <Box mx="auto" w="full" maxW="1060px" py={8} px={4}>
+        <SelectNational />
+        <CurrencyWrap currencyObjData={currencyObjData} />
+      </Box>
+    </>
   );
-}
+};
+
+export default Home;
